911df00ee9757e505d000f2244475b642c292f02
GreenDevth <theerapong@rumail.ru.ac.th>
2022-03-06 00:23:09 +0700

    add discord store

--- cogs/fishing_tools.py
+++ cogs/fishing_tools.py
@@ -1,6 +1,10 @@
 import discord
+import random
 from discord.ext import commands
 from discord_components import Button, ButtonStyle
+from database.Store_db import check_queue, add_to_shoping_cart, in_order
+from database.Players import players_info
+
 
 class FishingTools(commands.Cog):
     def __init__(self, bot):
@@ -8,20 +12,54 @@ class FishingTools(commands.Cog):
 
     @commands.Cog.listener()
     async def on_button_click(self, interaction):
-        
+        member = interaction.author
+        btn = interaction.component.custom_id
+        fishing_list = ["fishing_gray_set", "fishing_blue_set", "fishing_green_set", "fishing_red_set"]
+        run_channel = self.bot.get_channel(927796274676260944)
+        cmd_channel = self.bot.get_channel(925559937323659274)
+        code = random.randint(9, 99999)
+        order_number = f'#{code}'
+        count = check_queue()
+        queue = check_queue()
+
+        """ Golbal"""
+        message = None
+
+        """ Fishing Pack """
+
+        if btn in fishing_list:
+            player = players_info(member.id)
+            package_name = "dailypack"
+            order = in_order(player[2])
+            add_to_shoping_cart(member.id, member.name, player[3], order_number, package_name)
+            if count == 0:
+                message = f"กรุณารอสักครู่ ระบบกำลังเตรียมจัดส่ง เซ็ตอุปกรณ์ตกปลา {btn} ให้คุณ"
+                checkout = '--run {}'.format(order_number)
+                await run_channel.send(
+                    f'{member.mention}\n'
+                    f'```เลขที่ใบสั่งซื้อ {order_number} อยู่ระหว่างการจัดส่ง อยู่ในคิวจัดส่ง {order}/{queue}```')
+                await run_channel.send(checkout)
+            else:
+                message = f"กรุณารอสักครู่ ระบบกำลังเตรียมจัดส่ง เซ็ตอุปกรณ์ตกปลา {btn} ให้คุณ"
+                await run_channel.send(
+                    f'{member.mention}\n'
+                    f'```เลขที่ใบสั่งซื้อ {order_number} อยู่ระหว่างการจัดส่ง อยู่ในคิวจัดส่ง {order}/{queue}```')
+        await interaction.respond(content=message)
+
     @commands.command(name='fishing_pack')
     async def fishing_pack(self, ctx):
         await ctx.send(
             file=discord.File('./img/store/fishing_rod.png'),
             components=[
                 [
-                    Button(style=ButtonStyle.gray, label='GRAY SET', custom_id='gray_set'),
-                    Button(style=ButtonStyle.blue, label='BLUE SET', custom_id='blue_set'),
-                    Button(style=ButtonStyle.green, label='GREEN SET', custom_id='green_set'),
-                    Button(style=ButtonStyle.red, label='RED SET', custom_id='red_set')
+                    Button(style=ButtonStyle.gray, label='GRAY SET', custom_id='fishing_gray_set'),
+                    Button(style=ButtonStyle.blue, label='BLUE SET', custom_id='fishing_blue_set'),
+                    Button(style=ButtonStyle.green, label='GREEN SET', custom_id='fishing_green_set'),
+                    Button(style=ButtonStyle.red, label='RED SET', custom_id='fishing_red_set')
                 ]
             ]
         )
+
 
 def setup(bot):
     bot.add_cog(FishingTools(bot))
---
+++ database/Store_db.py
@@ -0,0 +1,92 @@
+from mysql.connector import MySQLConnection, Error
+from database.db_config import read_db_config
+
+db = read_db_config()
+
+
+def in_order(discord_id):
+    """Count product_code from current discord id"""
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute('SELECT COUNT(order_number) FROM scum_shopping_cart_demo WHERE discord_id = %s', (discord_id,))
+        row = cur.fetchone()
+        while row is not None:
+            order = list(row)
+            return order[0]
+    except Error as e:
+        print(e)
+
+
+def check_queue():
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute("select count(*) from scum_shopping_cart_demo")
+        row = cur.fetchone()
+        res = list(row)
+        return res[0]
+    except Error as e:
+        print(e)
+
+
+def add_to_shoping_cart(discord_id, discord_name, steam_id, order_number, package_name):
+    conn = None
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO scum_shopping_cart_demo(discord_id, discord_name, steam_id, order_number, package_name) "
+            "VALUES (%s,%s,%s,%s,%s)",
+            (discord_id, discord_name, steam_id, order_number, package_name))
+        conn.commit()
+        cur.close()
+        return False
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            conn.close()
+
+
+def delete_row():
+    conn = None
+    try:
+
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute('DELETE FROM scum_shopping_cart_demo LIMIT 1')
+        conn.commit()
+        cur.close()
+    except Error as e:
+        print(e)
+    finally:
+        if conn.is_connected():
+            conn.close()
+
+
+def get_package(pack_name):
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute('SELECT package_data FROM scum_package WHERE package_name = %s', (pack_name,))
+        row = cur.fetchone()
+        while row is not None:
+            data = list(row)
+            return data[0]
+    except Error as e:
+        print(e)
+
+
+def get_queue(product_code):
+    try:
+        conn = MySQLConnection(**db)
+        cur = conn.cursor()
+        cur.execute('SELECT steam_id, package_name FROM scum_shopping_cart_demo WHERE order_number = %s',
+                    (product_code,))
+        row = cur.fetchone()
+        while row is not None:
+            data = list(row)
+            return data
+    except Error as e:
+        print(e)
