f72716e3f1d3da63ce58f6427c900ab4c503c3dc
GreenDevth <theerapong@rumail.ru.ac.th>
2022-03-22 18:49:54 +0700

    edit a simple item manager

--- Store/ItemManager.py
+++ Store/ItemManager.py
@@ -7,7 +7,7 @@ class ItemsManager(commands.Cog):
 
     @commands.command(name='check')
     async def check_command(self, ctx, arg: str):
-        if ctx.channel.id != 925559937323659274 and ctx.author.guild_permissions.administrator:
+        if ctx.channel.id != 925559937323659274:
             await ctx.reply('คุณควรใช้งานคำสั่งนี้ที่ห้อง <#925559937323659274>')
             return False
 
